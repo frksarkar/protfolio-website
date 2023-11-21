4515e13c0ecca98d793c906bb3b4e53fc8d7c64f
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-11-21 13:49:28 +0600

    add skill section functionality

--- script.js
+++ script.js
@@ -2,14 +2,32 @@ const menuBtn = document.querySelector('.menu-btn');
 const closeBtn = document.querySelector('.close-btn');
 const menuBar = document.querySelector('.menu-bar');
 
+function toggleElement(e) {
+	e.querySelector('.items').classList.toggle('show');
+}
+
+const skillsSection = (() => {
+	const skills = document.querySelectorAll('#skills .skill');
+	skills.forEach((element) => {
+		element.addEventListener('click', function () {
+			toggleElement(this);
+		});
+	});
+	return { skills };
+})();
+
 menuBtn.addEventListener('click', () => {
-	menuBar.style.display = 'block';
+	// menuBar.style.display = 'block';
+	menuBar.classList.add('active');
+
 	menuBtn.style.display = 'none';
 	closeBtn.style.display = 'block';
 });
 
 closeBtn.addEventListener('click', () => {
-	menuBar.style.display = 'none';
+	// menuBar.style.display = 'none';
+	menuBar.classList.remove('active');
+
 	menuBtn.style.display = 'block';
 	closeBtn.style.display = 'none';
 });
